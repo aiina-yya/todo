57ec093fac2678ec00ae08dfea97ee1ec804d91a
aiina <aina19920331@gmail.com>
2026-02-19 15:35:20 +0900

    2-6

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -20,6 +20,20 @@ class TodoController extends Controller
         $todo = $request->only(['content']);
         Todo::create($todo);
 
-        return redirect('/')->with('success','Todoを作成しました');
+        return redirect('/')->with('message','Todoを作成しました');
+    }
+
+    public function update(TodoRequest $request)
+    {
+        $todo = $request->only(['content']);
+        unset($todo['_token']);
+        Todo::find($request->id)->update($todo);
+        return redirect('/')->with('message', 'Todoを更新しました');
+    }
+
+    public function destroy(Request $request)
+    {
+        Todo::find($request->id)->delete();
+        return redirect('/')->with('massage', 'Todoを削除しました');
     }
 }
