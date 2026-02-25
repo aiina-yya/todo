70a860ee3f7b43aade6c8eb4da89f80035c51a3e
aiina <aina19920331@gmail.com>
2026-02-26 03:49:19 +0900

    3-5

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,11 +16,9 @@ use App\Http\Controllers\CategoryController;
 */
 
 Route::get('/', [TodoController::class, 'index']);
-
 Route::post('/todos', [TodoController::class, 'store']);
-
 Route::patch('/todos/update', [TodoController::class, 'update']);
-
 Route::delete('/todos/delete',[TodoController::class,'destroy']);
 
 Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/categories', [CategoryController::class, 'store']);
